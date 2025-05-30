19c0e6cbdca4aa6d38e1f5cb064255880be2c3b2
Masih Ghaznavi <masihprogrammer@gmail.com>
2025-05-30 13:50:27 +0330

    fix bundler and scripts

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,20 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  root: '.', // default root
+
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'BankUtils', // Global var name in UMD build
+      name: 'BankUtils',
       fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
-      external: [], // externalize dependencies like 'lodash'
-      output: {
-        globals: {}, // for UMD builds if needed
-      },
+      external: [],
     },
   },
+  server: {
+    open: false,
+  }
 });
